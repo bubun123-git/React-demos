ca6f338917df4cd4597aae64c6b6f120e1667fb3
bubun123-git <bubundcool@gmail.com>
2023-07-07 13:38:20 +0530

    added button and input handler

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AddUser from './Components/Users/AddUsers';
+
+function App() {
+  return (
+    <div>
+      <AddUser></AddUser>
+    </div>
+  );
+}
+
+export default App;
